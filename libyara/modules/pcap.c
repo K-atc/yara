7b165837f2acc28e2654e6cfc5df93f8d0bde2d0
K_atc <shiftx1026@gmail.com>
2017-07-08 04:01:19 +0900

    Add: handles wireshark json format & http support

--- libyara/modules/pcap.c
+++ libyara/modules/pcap.c
@@ -34,24 +34,132 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define MODULE_NAME pcap
 
+
+#define METHOD_GET    0x01
+#define METHOD_POST   0x02
+
+uint64_t pcap_http_request(
+    YR_OBJECT* network_obj,
+    RE* uri_regexp,
+    int methods)
+{
+  json_t* json = (json_t*) network_obj->data;
+  json_t* packet;
+
+  uint64_t result = 0;
+  size_t i;
+
+  const char* method;
+  const char* full_uri;
+
+  json_array_foreach(json, i, packet) {
+    json_t* layers = json_object_get(json_object_get(packet, "_source"), "layers");
+    json_t* http = json_object_get(layers, "http");
+    if(http)
+    {
+      void *iter = json_object_iter(http);
+      if(iter)
+      {
+        json_t* value = json_object_iter_value(iter);
+        json_t* obj_method = json_object_get(value, "http.request.method");
+        if(obj_method)
+        {
+          method = json_string_value(obj_method);
+        }
+        else
+        {
+          method = "get";
+        }
+      }
+      else
+      {
+        method = "get";
+      }
+      json_t* obj_full_uri = json_object_get(http, "http.request.full_uri");
+      if(obj_full_uri)
+      {
+        full_uri = json_string_value(obj_full_uri);
+        if (((methods & METHOD_GET && strcasecmp(method, "get") == 0) ||
+             (methods & METHOD_POST && strcasecmp(method, "post") == 0)) &&
+             yr_re_match(uri_regexp, full_uri) > 0)
+        {
+          result = 1;
+          break;
+        }        
+      }
+
+    }
+  }
+
+  return result;
+}
+
+define_function(check_http_request)
+{
+  return_integer(
+      pcap_http_request(
+          parent(),
+          regexp_argument(1),
+          METHOD_GET | METHOD_POST));
+}
+
+
+define_function(check_http_get)
+{
+  return_integer(
+      pcap_http_request(
+          parent(),
+          regexp_argument(1),
+          METHOD_GET));
+}
+
+
+define_function(check_http_post)
+{
+  return_integer(
+      pcap_http_request(
+          parent(),
+          regexp_argument(1),
+          METHOD_POST));
+}
+
+
 begin_declarations;
 
   declare_string("greeting");
 
   declare_integer("number_of_packets");
 
+  begin_struct("check");
+    declare_function("http_get", "r", "i", check_http_get);
+    declare_function("http_post", "r", "i", check_http_post);
+    declare_function("http_request", "r", "i", check_http_request);
+  end_struct("check");
+
   begin_struct_array("packets");
+
     begin_struct("tcp");
-        declare_integer("srcport");
-        declare_integer("dstport");
+      declare_integer("srcport");
+      declare_integer("dstport");
     end_struct("tcp");
+  
     begin_struct("ip");
-        declare_integer("src");
-        declare_integer("dst");
+      declare_integer("src");
+      declare_integer("dst");
     end_struct("ip");
+  
+    begin_struct("http");
+      begin_struct("request");
+        declare_string("method");
+        declare_string("uri");
+        declare_string("host");
+        declare_string("full_uri");
+      end_struct("request");
+    end_struct("http");
   end_struct_array("packets");
 
 end_declarations;
+
 
 
 int module_initialize(
@@ -66,7 +174,6 @@ int module_finalize(
 {
   return ERROR_SUCCESS;
 }
-
 
 int module_load(
     YR_SCAN_CONTEXT* context,
@@ -104,7 +211,10 @@ int module_load(
       return ERROR_INVALID_FILE;
     }
 
+    // FIX ME: Not so good idea
     module_object->data = (void*) json;
+    YR_OBJECT* check_object = get_object(module_object, "check");
+    check_object->data = (void*) json;
 
     size_t num_packets = json_array_size(json);
     set_integer(num_packets, module_object, "number_of_packets");
@@ -112,8 +222,10 @@ int module_load(
     json_t* packet;
     int i;
     json_array_foreach(json, i, packet) {
-      json_t* tcp_srcport = json_object_get(packet, "tcp.srcport");
-      json_t* tcp_dstport = json_object_get(packet, "tcp.dstport");
+      json_t* layers = json_object_get(json_object_get(packet, "_source"), "layers");
+      json_t* tcp = json_object_get(layers, "tcp");
+      json_t* tcp_srcport = json_object_get(tcp, "tcp.srcport");
+      json_t* tcp_dstport = json_object_get(tcp, "tcp.dstport");
       if(tcp_srcport && tcp_dstport)
       {      
         // // printf("tcp.srcport = %" JSON_INTEGER_FORMAT " , tcp.dstport = %" JSON_INTEGER_FORMAT "\n", 
@@ -125,6 +237,36 @@ int module_load(
         set_integer(atoi(json_string_value(tcp_srcport)), module_object, "packets[%i].tcp.srcport", i);
         set_integer(atoi(json_string_value(tcp_dstport)), module_object, "packets[%i].tcp.dstport", i);
       }
+      json_t* http = json_object_get(layers, "http");
+      if(http)
+      {
+        json_t* host = json_object_get(http, "http.host");
+        if(host)
+        {
+          set_string(json_string_value(host), module_object, "packets[%i].http.request.host", i);
+        }
+        json_t* full_uri = json_object_get(http, "http.request.full_uri");
+        if(full_uri)
+        {
+          // printf("[%d] full_uri = %s\n", i, json_string_value(full_uri));
+          set_string(json_string_value(full_uri), module_object, "packets[%i].http.request.full_uri", i);
+        }
+
+        void *iter = json_object_iter(http);
+        if(iter)
+        {
+          json_t* value = json_object_iter_value(iter);
+          json_t* method = json_object_get(value, "http.request.method");
+          if(method)
+          {
+            set_string(json_string_value(method), module_object, "packets[%i].http.request.method", i);
+          }
+          json_t* uri = json_object_get(value, "http.request.uri");
+          if(uri){
+            set_string(json_string_value(uri), module_object, "packets[%i].http.request.uri", i);
+          }
+        }
+      }
     }
   }
 
@@ -132,8 +274,10 @@ int module_load(
 }
 
 
-int module_unload(
-    YR_OBJECT* module_object)
+int module_unload(YR_OBJECT* module)
 {
+  if (module->data != NULL)
+    json_decref((json_t*) module->data);
+
   return ERROR_SUCCESS;
 }
